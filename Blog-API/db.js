cb77f2de8e6a873f036f7c520d31095c4b811bac
BaoWoW <baonguyen9392@gmail.com>
2023-08-06 18:42:00 -0700

    Setup Database With Minimal Login Authentication
    Created an Author and Post Schema with basic GET/POST route handlers.
    Author signup/login is minimally authenticated with bcrypt.
    Forbidded signup route to only allow me to create posts.
    JWT not implemented for POST login.

---
+++ Blog-API/db.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const authorSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { collection: "Authors" }
+);
+
+const postSchema = new Schema(
+  {
+    title: { type: String, required: true, minlength: 1, maxlength: 50 },
+    content: { type: String, required: true, minlength: 1, maxlength: 1000 },
+    date: { type: Date, required: true },
+    author: { type: Schema.ObjectId, required: true, ref: "Author" },
+  },
+  { collection: "Posts" }
+);
+
+authorSchema.virtual("url").get(function () {
+  return `/author/${this._id}`;
+});
+
+postSchema.virtual("formatDate").get(function () {
+  if (this.date) {
+    return DateTime.fromJSDate(this.date).toFormat("MM-dd-yy hh:mm a");
+  }
+  return null;
+});
+
+const Post = mongoose.model("Post", postSchema);
+const Author = mongoose.model("Author", authorSchema);
+
+module.exports = { Post, Author };
--- Blog-API/routes/index.js
+++ Blog-API/routes/index.js
@@ -1,9 +1,50 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+const bcrypt = require("bcryptjs");
+const { Author } = require("../db");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Blog" });
+});
+
+router.get("/signup", (req, res, next) => {
+  res.sendStatus(403); //No one can sign up, only my blog!
+});
+
+router.post("/signup", function (req, res, next) {
+  res.sendStatus(403);
+  bcrypt.hash(req.body.password, 10, async (err, hashedPw) => {
+    if (err) return next(err);
+    const author = new Author({
+      username: req.body.username,
+      password: hashedPw,
+    });
+    const save = await author.save();
+  });
+  res.render("index");
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("login", { title: "Log in" });
+});
+
+router.post("/login", async function (req, res, next) {
+  const author = await Author.findOne({ username: req.body.username })
+    .populate("password")
+    .exec();
+  if (!author) return res.send("No author");
+  const pw = author.password;
+  try {
+    bcrypt.compare(req.body.password, pw, async (err, isMatch) => {
+      if (err) return next(err);
+      if (isMatch) {
+        //Make sure to use JWT to keep session later
+        res.render("index", { title: `Welcome ${author.username}` });
+      } else res.send('Wrong password')
+    });
+  } catch (err) {
+    res.send('Error comparing passwords')
+  }
 });
 
 module.exports = router;
